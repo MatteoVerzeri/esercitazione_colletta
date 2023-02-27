1f3af22eb9d47e66d237275b1cb513f97b80432b
MatteoVerzeri <matverzer@gmail.com>
2023-02-27 10:35:37 +0100

    Aggiungere i file di progetto.

---
+++ esercitazione_colletta/colletta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercitazione_colletta
+{
+    public class colletta
+    {
+        Dictionary<string, double> persone = new Dictionary<string, double>();
+        public void aggiunta(double cifra, string nome)
+        {
+            persone[nome] += cifra;
+        }
+        public void rimozione(double cifra, string nome)
+        {
+            persone[nome] -= cifra;
+        }
+        public void modifica(double cifra, string nome)
+        {
+            persone[nome] = cifra;
+        }
+        public void visualizza()
+        {
+            Dictionary<string, double>.KeyCollection keyColl = persone.Keys;
+
+            string a = "";
+            foreach (string s in keyColl)
+            {
+                a += s.ToString();
+            }
+        }
+    }
+}
