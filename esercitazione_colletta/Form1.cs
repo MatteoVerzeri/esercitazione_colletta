6583dc788eee78a81f1270f229263c5c4325bc6e
MatteoVerzeri <matverzer@gmail.com>
2023-02-27 10:53:46 +0100

    finito

--- esercitazione_colletta/Form1.cs
+++ esercitazione_colletta/Form1.cs
@@ -12,6 +12,28 @@ namespace esercitazione_colletta
 {
     public partial class Form1 : Form
     {
+        Dictionary<string, double> persone = new Dictionary<string, double>();
+        public void aggiunta(double cifra, string nome)
+        {
+            persone[nome] = cifra;
+        }
+        public void rimozione(double cifra, string nome)
+        {
+            persone.Remove(nome);
+        }
+        public void visualizza()
+        {
+            Dictionary<string, double>.KeyCollection keyColl = persone.Keys;
+
+            string a = null;
+            foreach(KeyValuePair<string, double> s in persone)
+            {
+                a += s.Key + Convert.ToDouble(s.Value) + "\n";
+            }
+                
+            
+            MessageBox.Show(a);
+        }
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +41,27 @@ namespace esercitazione_colletta
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            aggiunta(double.Parse(textBox2.Text), textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rimozione(double.Parse(textBox2.Text), textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            aggiunta(double.Parse(textBox2.Text), textBox1.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            visualizza();
         }
     }
 }
